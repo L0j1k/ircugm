03090247dff24789f7c89caa6a631cf64a2e393a
L0j1k <L0j1k@L0j1k.com>
2014-01-20 02:45:39 -0600

    added stubs

--- src/ircugm.cpp
+++ src/ircugm.cpp
@@ -5,7 +5,8 @@
  * operations on the data so that it may be more easily visualized.
  * ircugm will attempt to "learn" things about the users in its assigned 
  * channel and make a certain amount of predictions about the users 
- * based on previous behavior and other metrics.
+ * based on previous behavior and other metrics. It also produces pretty
+ * output from the analyses and reports.
  * 
  * ircugm is written by L0j1k (L0j1k@L0j1k.com) and released under the 
  * BSD-3 software license.
@@ -13,7 +14,7 @@
  * @author L0j1k (l0j1k@l0j1k.com)
  * @version 0.0.1a
  * @created 23may2013
- * @modified 17jan2014
+ * @modified 20jan2014
  */
 
 #include <iostream>
@@ -25,7 +26,6 @@ bool getOptions() {
 }
 
 void usage() {
-
 }
 
 int main( int argc, char** argv ) {
---
+++ src/ircugm.h
@@ -0,0 +1,22 @@
+/**
+ * ircugm -- IRC User Grapher/Mapper
+ * This utility is automated software used to idle in IRC servers and 
+ * collect information about the users in a channel, then perform 
+ * operations on the data so that it may be more easily visualized.
+ * ircugm will attempt to "learn" things about the users in its assigned 
+ * channel and make a certain amount of predictions about the users 
+ * based on previous behavior and other metrics. It also produces pretty
+ * output from the analyses and reports.
+ * 
+ * ircugm is written by L0j1k (L0j1k@L0j1k.com) and released under the 
+ * BSD-3 software license.
+ * 
+ * @author L0j1k (l0j1k@l0j1k.com)
+ * @version 0.0.1a
+ * @created 23may2013
+ * @modified 17jan2014
+ */
+
+bool getOptions(void);
+
+void usage(void);
