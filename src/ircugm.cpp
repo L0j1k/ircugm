6e185fdadd357f47a11799d7dff8d9827ff06b7d
L0j1k <L0j1k@L0j1k.com>
2014-01-20 01:43:16 -0600

    refactored project file structure

---
+++ src/ircugm.cpp
@@ -0,0 +1,38 @@
+/**
+ * ircugm -- IRC User Grapher/Mapper
+ * This utility is automated software used to idle in IRC servers and 
+ * collect information about the users in a channel, then perform 
+ * operations on the data so that it may be more easily visualized.
+ * ircugm will attempt to "learn" things about the users in its assigned 
+ * channel and make a certain amount of predictions about the users 
+ * based on previous behavior and other metrics.
+ * 
+ * ircugm is written by L0j1k (L0j1k@L0j1k.com) and released under the 
+ * BSD-3 software license.
+ * 
+ * @author L0j1k (l0j1k@l0j1k.com)
+ * @version 0.0.1a
+ * @created 23may2013
+ * @modified 17jan2014
+ */
+
+#include <iostream>
+
+using namespace std;
+
+bool getOptions() {
+  return true;
+}
+
+void usage() {
+
+}
+
+int main( int argc, char** argv ) {
+
+  if(!getOptions()) {
+    usage();
+  }
+
+  return 0;
+}
